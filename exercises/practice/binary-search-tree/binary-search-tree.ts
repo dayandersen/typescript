5c4067aa52bb40117e77fdde02eae94737bd7fd6
dandersen <adayne@amazon.com>
2025-05-24 03:38:53 -0700

    I don't like how I did my binary search for each exploration and callback. Currently I return a list of all elements and then apply the callback over those. Pretty sure I can just apply the callback as I recurse?

--- exercises/practice/binary-search-tree/binary-search-tree.ts
+++ exercises/practice/binary-search-tree/binary-search-tree.ts
@@ -1,25 +1,60 @@
 export class BinarySearchTree {
-  constructor(data: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private readonly val: number;
+  private r: BinarySearchTree | undefined = undefined;
+  private l: BinarySearchTree | undefined = undefined;
+  private parent: BinarySearchTree | undefined
+
+
+
+  constructor(data: number, parent: BinarySearchTree | undefined = undefined) {
+    this.val = data;
+    this.parent = parent
   }
 
-  public get data(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public get data(): number {
+    return this.val
   }
 
   public get right(): BinarySearchTree | undefined {
-    throw new Error('Remove this statement and implement this function')
+    return this.r
   }
 
   public get left(): BinarySearchTree | undefined {
-    throw new Error('Remove this statement and implement this function')
+    return this.l
   }
 
-  public insert(item: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public insert(item: number): void {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    let curr: BinarySearchTree = this
+    
+    while (true) {
+      if (item > curr.data) {
+        if (curr.right === undefined) {
+          curr.r = new BinarySearchTree(item, curr)
+          return
+        } else {
+          curr = curr.r!
+        }
+      } else {
+        if (curr.left === undefined) {
+          curr.l = new BinarySearchTree(item, curr)
+          return
+        } else {
+          curr = curr.l!
+        }
+      }
+    }
   }
 
-  public each(callback: (data: unknown) => unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public each(callback: (data: number) => unknown) {
+    this.recurse(this).forEach((it: number) => callback(it))
+  }
+
+  private recurse(curr: BinarySearchTree | undefined): number[] {
+    if (curr === undefined) {
+      return []
+    } else {
+      return this.recurse(curr.l).concat([curr.val]).concat(this.recurse(curr.r))
+    }
   }
 }
